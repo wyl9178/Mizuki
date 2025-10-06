1135f6834df35caa6c075936b516276c48439746
yCENzh <166907745+yCENzh@users.noreply.github.com>
2025-10-06 17:14:04 +0800

    4

--- src/config.ts
+++ src/config.ts
@@ -27,7 +27,7 @@ export const siteConfig: SiteConfig = {
 	lang: SITE_LANG,
 
 	themeColor: {
-		hue: 200, // 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
+		hue: 160, // 主题色的默认色相，范围从 0 到 360。例如：红色：0，青色：200，蓝绿色：250，粉色：345
 		fixed: false, // 对访问者隐藏主题色选择器
 	},
 
